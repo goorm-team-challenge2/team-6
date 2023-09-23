6419424bd226ff9452c30932ee31dd5a6209c4a3
Gukin Han <gukin.dev@gmail.com>
2023-09-23 16:57:24 +0900

    app 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { EmptyView, Header } from '@/components';
+import React, { useState } from 'react';
+import ResultView from './components/ResultView/ResultView';
 
 import styles from './App.module.scss';
 
 function App() {
-	return (
-		<div className={styles.App}>
-			<Header />
-			<main className={styles.main}>
-				<EmptyView />
-			</main>
-		</div>
-	);
+    const [formResults, setFormResults] = useState([1]);
+
+    return (
+        <div className={styles.App}>
+            <Header />
+            <main className={styles.main}>
+                {formResults.length === 0 ? (
+                    <EmptyView />
+                ) : (
+                    <ResultView formResults={formResults} />
+                )}
+            </main>
+        </div>
+    );
 }
 
 export default App;
