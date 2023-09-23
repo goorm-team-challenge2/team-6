62599be43b50af7b361a6144cbceaf790873adad
Daeeun Ko <7273322@naver.com>
2023-09-23 14:39:08 +0900

    feat: 모달창 생성

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,25 @@
 import cn from 'classnames';
 
-import { Button, Typography } from '@goorm-dev/gds-challenge';
+import { Typography } from '@goorm-dev/gds-challenge';
 
 import styles from './Header.module.scss';
+import CustomModal from "@/components/Modal/CustomModal";
+import {useState} from "react";
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<header className={cn(styles.header)}>
 			<div className={cn(styles.contents)}>
 				<Typography token="h5">
 					구름톤 챌린지 참여자 정보 수집
 				</Typography>
-				<Button size="lg">설문조사 참여하기</Button>
+				<CustomModal isOpen={isOpen} toggle={toggle} />
 			</div>
 		</header>
 	);
---
+++ src/components/Modal/CustomModal.jsx
@@ -0,0 +1,18 @@
+import { Button, Modal } from '@goorm-dev/gds-challenge';
+
+const CustomModal = ({ isOpen, toggle }) => {
+	return (
+		<>
+			<Button size="lg" onClick={toggle}>설문조사 참여하기</Button>
+			<Modal isOpen={isOpen} toggle={toggle}>
+				<Modal.Header toggle={toggle}>헤더</Modal.Header>
+				<Modal.Body>컨텐츠를 넣어주세요.</Modal.Body>
+				<Modal.Footer>
+					<Button>다음</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+};
+
+export default CustomModal;
